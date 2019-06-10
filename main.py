d88febaef1149cb985868153b11ac4a73a237a3e
Anna Poletti <34551669+atk03@users.noreply.github.com>
2019-06-10 17:07:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from functions import get_cell, wait_for_ajax, readonly_check, wait_until
-from pages import Model, Company, HWCategory, Quantity
+from pages import MyClass
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
@@ -25,79 +25,43 @@ for driver in drivers:
     passed = 0
 
     #page to test
-    model = Model(driver)
-    company = Company(driver)
-    hw_category = HWCategory(driver)
-    quantity = Quantity(driver)
+    my_id = MyClass(driver)
     print('--------- TEST 0: input permission ---------')
 
-    if readonly_check(model.input_form):
+    if readonly_check(my_id.input_form):
         passed += 1
     else:
         failed += 1
-        print('fail on: model')
+        print('fail on: my_id')
 
-    if readonly_check(company.input_form):
-        passed += 1
-    else:
-        failed += 1
-        print('fail on: company')
-
-    if readonly_check(hw_category.input_form):
-        passed += 1
-    else:
-        failed += 1
-        print('fail on: hw_category')
-
-    if readonly_check(quantity.input_form):
-        failed += 1
-        print('fail on: quantity')
-    else:
-        passed += 1
+   
 
     print('--------- TEST 1: open - close table modal ---------')
 
-    model.modal_btn.click()
+    my_id.modal_btn.click()
     wait_for_ajax(driver)
-    if model.table == driver.find_element_by_id('model-name'):
+    if my_id.table == driver.find_element_by_id('my_id'):
         passed += 1
     else:
         failed += 1
-        print('fail on: model')
+        print('fail on: my_id')
 
-    model.x_btn.click()
+    my_id.x_btn.click()
     wait_until(driver, By.XPATH, "//body[@class='']")
     if driver.find_element_by_tag_name('body').get_attribute('class') == '':
         passed += 1
     else:
         failed += 1
-        print('fail on: model')
+        print('fail on: my_id')
     driver.refresh()
 
-    print('--------- TEST 2: datatable features ---------')
+    print('--------- TEST 2:  features ---------')
 
-    model.modal_btn.click()
+    my_id.modal_btn.click()
     wait_for_ajax(driver)
-    model.select_length.click()
-    wait_for_ajax(driver)
-    string_test = 'a'
-    model.general_search = string_test
-    model.general_search.clear()
-    wait_for_ajax(driver)
-    model.column_search = 'e'
-    wait_for_ajax(driver)
-    model.sort_btn.click()
-    wait_for_ajax(driver)
-    single_cell = get_cell(model.table, 3, 0)
+    single_cell = get_cell(my_id.table, 3, 0)
     value = single_cell.text
-    single_cell.click()
-    wait_for_ajax(driver)
-    model.confirm_btn.click()
-    wait_for_ajax(driver)
-    if model.input_form.get_attribute("value") == value and model.input_type.get_attribute("value") != '' and model.input_manufacturer.get_attribute("value") != '':
-        passed += 1
-    else:
-        print('Input is wrong')
+
 
     print('--------- TEST 3: modal btn ---------')
 
@@ -105,8 +69,8 @@ for driver in drivers:
     wait_for_ajax(driver)
     model.cancel_btn.click()
     wait_until(driver, By.XPATH, "//body[@class='']")
-    if driver.find_element_by_tag_name('body').get_attribute('class') == '':
+    if my_id.find_element_by_tag_name('body').get_attribute('class') == '':
         passed += 1
     else:
         failed += 1
-        print('fail on: model')
+        print('fail on: my_id')
